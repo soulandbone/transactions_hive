543df67ed149ab97a9077fda0bf10cdd4f9cf9f7
SoulAndBone <soulandbone@gmail.com>
2023-05-29 10:16:14 +0300

    added style changes to the Transaction Form

--- lib/widgets/transaction_dialog.dart
+++ lib/widgets/transaction_dialog.dart
@@ -25,10 +25,34 @@ class _TransactionDialogState extends State<TransactionDialog> {
             ),
           ),
           TextFormField(
-            decoration: const InputDecoration(hintText: 'Enter Name'),
+            validator: (value) {
+              if (value!.isEmpty || value.length < 2) {
+                return 'Please submit a valid name for the TRX';
+              } else {
+                return null;
+              }
+            },
+            decoration: const InputDecoration(
+                hintText: 'Enter Name',
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 1, color: Colors.black54))),
           ),
+          const Gap(10),
           TextFormField(
-            decoration: const InputDecoration(hintText: 'Enter Amount'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter an amount';
+              } else if (double.parse(value) <= 0) {
+                return 'Please enter an amount greater than zero';
+              } else {
+                return null;
+              }
+            },
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+                hintText: 'Enter Amount',
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 1, color: Colors.black54))),
           ),
           const Gap(10),
           buildRadioOptions(),
@@ -61,6 +85,7 @@ class _TransactionDialogState extends State<TransactionDialog> {
 
   Widget buildButtonRow() {
     return const Row(
+      mainAxisAlignment: MainAxisAlignment.end,
       children: [
         TextButton(onPressed: null, child: Text('Cancel')),
         TextButton(onPressed: null, child: Text('Add'))
