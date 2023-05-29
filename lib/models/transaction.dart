c6744a433e8f90b8431b8db724fdadb322c6e476
SoulAndBone <soulandbone@gmail.com>
2023-05-29 17:35:26 +0300

    previous to the generation of the Adapter

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,1 +1,22 @@
+import 'package:hive/hive.dart';
 
+@HiveType(typeId: 0)
+class Transaction extends HiveObject {
+  @HiveField(0)
+  String name;
+
+  @HiveField(1)
+  DateTime createdDate;
+
+  @HiveField(2)
+  bool isExpense;
+
+  @HiveField(3)
+  double amount;
+
+  Transaction(
+      {required this.name,
+      required this.createdDate,
+      this.isExpense = true,
+      required this.amount});
+}
